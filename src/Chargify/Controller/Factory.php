b701a0397c49369ef3ed1b5904148de5465bbfec
root <darryl.kuhn@gmail.com>
2013-10-17 01:38:17 -0700

    Added support for instance calls to factory. This allows you to hydrate a factory once with credentials and then just make calls for controllers without specifying anything else

--- src/Chargify/Controller/Factory.php
+++ src/Chargify/Controller/Factory.php
@@ -8,31 +8,59 @@ use Exception;
 
 class Factory
 {
-  public static function build($type, $domain, $api_key) {
+    private $domain = null;
+    private $api_key = null;
 
-    // Get the base url for all the connections.
-    $base_url = sprintf('https://%s.chargify.com', $domain);
-
-    // Set the response format through the header.
-    $header = array(
-      'Content-Type' => 'application/json',
-      'Accept' => 'application/json'
-    );
-
-    // Add the same basic authentication to all requests.
-    $basicAuth = new CurlAuthPlugin($api_key, 'x');
-
-    $client = new Client($base_url);
-    $client->addSubscriber($basicAuth);
-    $client->setDefaultHeaders($header);
-
-    $class_name = 'Chargify\\Controller\\' . ucfirst($type);
-
-    if (class_exists($class_name)) {
-      return new $class_name($client);
+    /**
+     * If called as instance set some basic variables for later use.
+     */
+    public function __construct( $domain, $api_key )
+    {
+        $this->domain = $domain;
+        $this->api_key = $api_key;
     }
-    else {
-      throw new Exception("Invalid controller type given.");
+
+    /**
+     * Simulates an instance call but converts the instance method into the name
+     * parameter of a static build call 
+     * 
+     * eg: $factory->product() becomes Factory::Build('product', [])
+     * 
+     * @param string $name name of method
+     * @param array $arguments
+     * @return Chargify\Controller
+     */
+    public function __call( $name, $arguments )
+    {
+        return self::build($name, $this->domain, $this->api_key );
     }
-  }
+
+    public static function build($type, $domain, $api_key) 
+    {
+
+        // Get the base url for all the connections.
+        $base_url = sprintf('https://%s.chargify.com', $domain);
+
+        // Set the response format through the header.
+        $header = array(
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        );
+
+        // Add the same basic authentication to all requests.
+        $basicAuth = new CurlAuthPlugin($api_key, 'x');
+
+        $client = new Client($base_url);
+        $client->addSubscriber($basicAuth);
+        $client->setDefaultHeaders($header);
+
+        $class_name = 'Chargify\\Controller\\' . ucfirst($type);
+
+        if (class_exists($class_name)) {
+            return new $class_name($client);
+        }
+        else {
+            throw new Exception("Invalid controller type given.");
+        }
+    }
 }

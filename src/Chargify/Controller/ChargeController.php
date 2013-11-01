b5c92746b4c8c7e11bd5cee2faca1250fae2d42a
root <darryl.kuhn@gmail.com>
2013-10-31 20:10:06 -0400

    Added ability to do one off charges

---
+++ src/Chargify/Controller/ChargeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Chargify\Controller;
+
+use \Chargify\Resource\ChargeResource as Resource;
+
+class Charge extends AbstractController 
+{
+    /**
+     *
+     */
+    public function create( $subscriptionId, $data )
+    {
+        $response = $this->request('subscriptions/' . $id . '/charges' , $data, 'POST');
+        $charge = new Resource( $response['charge'] );
+
+        return $charge;
+    }
+}
---
+++ src/Chargify/Resource/ChargeResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Chargify\Resource;
+
+class ChargeResource extends AbstractResource 
+{
+    public $id;
+    public $success;
+    public $memo;
+    public $amount_in_cents;
+    public $ending_balance_in_cents;
+    public $type;
+    public $transaction_type;
+    public $success;
+    public $subscription_id;
+    public $product_id;
+    public $created_at;
+    public $payment_id;
+
+    public function getName() {
+        return 'charge';
+    }
+
+    public function getFilter() {
+        return array(
+            'created_at' => function($value) { return new \DateTime($value); },
+        );
+    }
+}
